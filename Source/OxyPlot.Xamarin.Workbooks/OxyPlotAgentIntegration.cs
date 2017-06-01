995fab96bc404028e537689d0f3b64501598ee1a
Aaron Bockover <abock@xamarin.com>
2017-06-01 18:48:28 -0400

    Initial Xamarin Workbooks integration

---
+++ Source/OxyPlot.Xamarin.Workbooks/OxyPlotAgentIntegration.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Interactive;
+
+using OxyPlot.Xamarin.Workbooks;
+
+[assembly: AgentIntegration (typeof (OxyPlotAgentIntegration))]
+
+namespace OxyPlot.Xamarin.Workbooks
+{
+    sealed class OxyPlotAgentIntegration : IAgentIntegration
+    {
+        public void IntegrateWith(IAgent agent)
+            => agent.RepresentationManager.AddProvider(
+                new OxyPlotRepresentationProvider());
+    }
+}
---
+++ Source/OxyPlot.Xamarin.Workbooks/OxyPlotRepresentationProvider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+using Xamarin.Interactive.Representations;
+
+namespace OxyPlot.Xamarin.Workbooks
+{
+    sealed class OxyPlotRepresentationProvider : RepresentationProvider
+    {
+        public override IEnumerable<object> ProvideRepresentations(object obj)
+        {
+            if (obj is PlotModel plotModel)
+                yield return Image.FromSvg(SvgExporter.ExportToString(
+                    plotModel,
+                    width: 800,
+                    height: 600,
+                    isDocument: true));
+        }
+    }
+}
